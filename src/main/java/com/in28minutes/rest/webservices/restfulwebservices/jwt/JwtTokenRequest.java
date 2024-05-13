057f8a6c328cd6c9247187fc18bbfd470457fc99
Priyanshu Ranjan <heyapriyanshu@gmail.com>
2024-05-14 02:10:39 +0530

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtTokenRequest.java
@@ -0,0 +1,27 @@
+package com.in28minutes.rest.webservices.restfulwebservices.jwt;
+
+public class JwtTokenRequest {
+    private String username;
+    private String password;
+
+    public JwtTokenRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.restfulwebservices.jwt;
+
+public class JwtTokenResponse {
+    private String token;
+
+    public JwtTokenResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
+
